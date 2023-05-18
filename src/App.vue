459c4dce0d4b985c268966358889f693fcaa9a3e
aitcq <tcqai290498@gmail.com>
2023-05-18 23:58:41 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <KanbanBoard/>
+</template>
+
+<script>
+import KanbanBoard from './components/KanbanBoard.vue'
+
+export default {
+  name: 'App',
+  components: {
+    KanbanBoard
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
